5432c5135b2beffd72a29c5c7ccfc414aa96aba3
Lu15-prog <luana.c.cruz8@aluno.senai.br>
2025-10-23 10:42:35 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+using Microsoft.EntityFrameworkCore;
+using SpotifyAPI.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Configura DbContext com SQL Server
+builder.Services.AddDbContext<SpotifyContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("SpotifyConnection")));
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
